f67e49ffe781662e856b70d27c3f15f9b28946a0
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2022-03-28 00:00:36 +0700

    Fix Bug

--- include/libs/phpmailer/class.phpmailer.php
+++ include/libs/phpmailer/class.phpmailer.php
@@ -544,7 +544,7 @@ class PHPMailer {
       if ($this->exceptions) {
         throw $e;
       }
-      echo $e->getMessage()."\n";
+      //echo $e->getMessage()."\n";
       return false;
     }
   }
