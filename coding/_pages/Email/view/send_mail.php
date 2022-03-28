eaee31979a161a580e4928bab1dda4ae5ae0da7d
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2022-03-28 15:39:09 +0700

    Fix Bug

--- coding/_pages/Email/view/send_mail.php
+++ coding/_pages/Email/view/send_mail.php
@@ -803,8 +803,8 @@ class send_mail extends _page{
 		}
 		
 		$url = 'https://'.HOSTNAME.'/'.URL.'/include/sending.php?';
-		$dt_read = $url.'object=send&send=sendMail_send&data='.$args['meta_id'].'-4-'.$args['ID'];
-		$dt_link = $url.'object=send&send=sendMail_send&data='.$args['meta_id'].'-5-'.$args['ID'];
+		$dt_read = $url.'object=sobad_send&send=sendMail_send&data='.$args['meta_id'].'-4-'.$args['ID'];
+		$dt_link = $url.'object=sobad_send&send=sendMail_send&data='.$args['meta_id'].'-5-'.$args['ID'];
 		
 		$read = '<img style="display:none;" src="'.$dt_read.'"></br>';	
 		$link = $dt_link.'&link=';
@@ -815,9 +815,9 @@ class send_mail extends _page{
 		$html = str_replace('href="', 'href="'.$link, $html);
 
 		// Mencari src image lokal --> replace
-		$dicari = 'src="\/' . URL . '\/';
+		$dicari = 'src=\"\/' . URL;
 		if(preg_match("/$dicari/", $html)) {
-			$replace = 'src="https://'. HOSTNAME . '/' . URL . '/';
+			$replace = 'src="https://'. HOSTNAME . '/' . URL;
             $html = str_replace($dicari, $replace, $html);
         }
 		
