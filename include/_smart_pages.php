8eb810c37898177645641173a79b05ccf534a98e
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2023-08-24 10:47:49 +0700

    Add format romawi Add smart page

---
+++ include/_smart_pages.php
@@ -0,0 +1,190 @@
+<?php
+
+abstract class _smart_page extends _page{
+
+	protected static $head_title = '';
+
+	protected static $breadcrumb = '';
+
+	protected static $modal = 2;
+
+	protected static $type_layout = 1; // 0 : basic , 1 : portlet , 2 : tabs
+
+	protected static $active = '';
+
+	protected static $data_tabs = array();
+
+	protected static $label_card = '';
+
+	protected static $action = 'action';
+
+	// ----------------------------------------------------------
+	// Layout category  ------------------------------------------
+	// ----------------------------------------------------------
+
+	protected static function _array(){
+		if(is_callable(array(new static(), '_array_default'))){
+			$array = static::_array_default();
+		}else{
+			$table = static::$table;
+			$array = sobad_table::_get_table($table);
+		}
+
+		return array_keys($array);
+	}
+
+	protected static function smart_table(){
+		$data = array();
+		$args = self::_array();
+		$type = self::$type;
+
+		$tp = str_replace('tab_', '', $type);
+		$tp = intval($tp);
+		
+		$kata = $_search = '';
+		$where = "AND type_testing='$tp'";
+		if(parent::$search){
+			$src = parent::like_search($args,$where);	
+			$cari = $src[0];
+			$where = $src[0];
+			$kata = $src[1];
+			$_search = $src[2];
+		}else{
+			$cari=$where;
+		}
+
+		$args = self::_gets_db($args, $where);
+
+		$data = array(
+            'kata'      => $kata,
+            'search'    => $_search,
+            'data'      => $args,
+            'type'		=> $type
+        );
+
+		return self::_loadView('table',$data);
+	}
+
+	private static function head_title(){
+		$args = array(
+			'title'	=> static::$head_title,
+			'link'	=> array(
+				0	=> array(
+					'func'	=> static::$object,
+					'label'	=> static::$breadcrumb,
+				)
+			),
+			'date'	=> false,
+			'modal'	=> static::$modal
+		); 
+		
+		return $args;
+	}
+
+	protected static function get_box(){
+		$data = static::table();
+		
+		$action = static::$action;
+		$action = empty($action) ? '' : static::{$action}(); 
+		$box = array(
+			'label'		=> static::$label_card,
+			'tool'		=> '',
+			'action'	=> $action,
+			'func'		=> 'sobad_table',
+			'data'		=> $data
+		);
+
+		return $box;
+	}
+
+	protected static function layout(){
+		$type = static::$type_layout;
+		if($type == 0){
+			if(is_callable(array(new static(), 'basic_layout'))){
+				return static::basic_layout();
+			}
+		}else if($type == 1){
+			return static::portlet_layout();
+		}else if($type == 2){
+			return static::tabs_layout();
+		}
+
+		return '';
+	}
+
+	protected static function portlet_layout(){
+		$box = self::get_box();
+		
+		$opt = array(
+			'title'		=> self::head_title(),
+			'style'		=> array(static::$object, '_style'),
+			'script'	=> array(static::$object, '_script')
+		);
+		
+		return portlet_admin($opt,$box);
+	}
+
+	protected static function tabs_layout(){
+		$active = empty(static::$active) ? static::$data_tabs[0] : static::$active;
+
+		self::$type = $active;
+        $box = self::get_box();
+
+        $data = static::$data_tabs;
+
+        $object = static::$table;
+        $tabs = array();
+        foreach ($data as $key => $val) {
+            $tabs[$key] = array(
+                'key'    => 'tab_' . $key,
+                'label'  => $val,
+                'qty'    => $object::count("status='$key'")
+            );
+        }
+        $tabs = array(
+        	'active'	=> static::$active,
+            'tab'    	=> $tabs,
+            'func'    	=> '_portlet',
+            'data'    	=> $box
+        );
+
+        $opt = array(
+            'title'        => self::head_title(),
+            'style'     => array(static::$object, '_style'),
+            'script'    => array(static::$object, '_script')
+        );
+        return tabs_admin($opt, $tabs);
+	}
+
+	// ----------------------------------------------------------
+	// Form data category ---------------------------------------
+	// ----------------------------------------------------------
+	protected static function add_form(){
+		$type = isset($_POST['type']) ? $_POST['type'] : '';
+		$data = self::_array_default();
+
+		$config =self::_loadView('form',array('data' => $data));
+		$data = array(
+			'title'		=> 'Tambah data module',
+			'link'		=> '_add_db',
+			'data'		=> $config,
+			'type'		=> $type
+		);
+
+		return sobad_asset::_loadView('form',$data);
+	}
+
+	protected static function edit_form($data=array()){
+		$type = isset($_POST['type']) ? $_POST['type'] : '';
+
+		$config =self::_loadView('form',array('data' => $data));
+		$data = array(
+			'title'		=> 'Edit data module',
+			'link'		=> '_update_db',
+			'data'		=> $config,
+			'type'		=> $type
+		);
+
+		return sobad_asset::_loadView('form',$data);
+	}  	
+}
--- include/format.php
+++ include/format.php
@@ -71,7 +71,7 @@ function format_number_currency($current,$nominal){
 	return $format;
 }
 
-function format_date_id($date){
+function format_date_id($date=''){
 	if(empty($date) || $date == '0000-00-00' || $date == '1970-01-01'){
 		return '-';
 	}
@@ -273,4 +273,87 @@ function format_terbilang_usd($number)
 	}
 
 	return ucwords($string);
+}
+
+function format_romawi($angka=1)
+{
+    $hsl = "";
+    if ($angka < 1 || $angka > 5000) { 
+        // Statement di atas buat nentuin angka ngga boleh dibawah 1 atau di atas 5000
+        $hsl = "Batas Angka 1 s/d 5000";
+    } else {
+        while ($angka >= 1000) {
+            // While itu termasuk kedalam statement perulangan
+            // Jadi misal variable angka lebih dari sama dengan 1000
+            // Kondisi ini akan di jalankan
+            $hsl .= "M"; 
+            // jadi pas di jalanin , kondisi ini akan menambahkan M ke dalam
+            // Varible hsl
+            $angka -= 1000;
+            // Lalu setelah itu varible angka di kurangi 1000 ,
+            // Kenapa di kurangi
+            // Karena statment ini mengambil 1000 untuk di konversi menjadi M
+        }
+    }
+
+
+    if ($angka >= 500) {
+        // statement di atas akan bernilai true / benar
+        // Jika var angka lebih dari sama dengan 500
+        if ($angka > 500) {
+            if ($angka >= 900) {
+                $hsl .= "CM";
+                $angka -= 900;
+            } else {
+                $hsl .= "D";
+                $angka-=500;
+            }
+        }
+    }
+    while ($angka>=100) {
+        if ($angka>=400) {
+            $hsl .= "CD";
+            $angka -= 400;
+        } else {
+            $angka -= 100;
+        }
+    }
+    if ($angka>=50) {
+        if ($angka>=90) {
+            $hsl .= "XC";
+            $angka -= 90;
+        } else {
+            $hsl .= "L";
+            $angka-=50;
+        }
+    }
+    while ($angka >= 10) {
+        if ($angka >= 40) {
+            $hsl .= "XL";
+            $angka -= 40;
+        } else {
+            $hsl .= "X";
+            $angka -= 10;
+        }
+    }
+    if ($angka >= 5) {
+        if ($angka == 9) {
+            $hsl .= "IX";
+            $angka-=9;
+        } else {
+            $hsl .= "V";
+            $angka -= 5;
+        }
+    }
+    while ($angka >= 1) {
+        if ($angka == 4) {
+            $hsl .= "IV"; 
+            $angka -= 4;
+        } else {
+            $hsl .= "I";
+            $angka -= 1;
+        }
+    }
+
+    return ($hsl);
 }
