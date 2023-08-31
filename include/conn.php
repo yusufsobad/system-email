6bc9dec4a06efc3bda83dcb1c287790adbcd9b1d
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2023-08-31 09:24:58 +0700

    Fix bug

--- include/conn.php
+++ include/conn.php
@@ -5,19 +5,16 @@ class conn extends _error{
 	public static function connect(){
 		global $DB_NAME;
 		$database = $DB_NAME;
-
-		$DB_NAME .= development == 0 ? '' : '_demo';
+		$database .= development == 0 ? '' : '_demo';
 
 		$server = SERVER;
 		$user = USERNAME;
 		$pass = PASSWORD;
-		$database = $DB_NAME;//constant("DB_NAME");
 
 		$conn=new mysqli($server,$user,$pass,$database);
 		mysqli_connect($server,$user,$pass) or parent::_connect();
 		$conn->select_db($database) or parent::_database();
-		
-		$DB_NAME = $database;
+
 		return $conn;
 	}
 }
