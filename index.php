2217dcb7f2d94793cda3671bf71d36f69dc710b6
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2024-03-09 13:13:13 +0700

    fix : notification const url firebase

--- index.php
+++ index.php
@@ -43,8 +43,8 @@ global $body;
 	<link rel="icon" type="image/ico" href="favicon.ico" /> 
 
 	<!-- Firebase notif -->	
-<!-- 	<script src="https://cdn.firebase.com/js/client/2.2.3/firebase.js"></script>
-	<script src="https://www.gstatic.com/firebasejs/3.6.1/firebase.js"></script> -->
+	<script src="https://cdn.firebase.com/js/client/2.2.3/firebase.js"></script>
+<!-- 	<script src="https://www.gstatic.com/firebasejs/3.6.1/firebase.js"></script> -->
 	
 	<?php
 		$asset->_vendor_css();
@@ -64,6 +64,9 @@ global $body;
     	const hosting = "<?= SITE.'://'.HOSTNAME ;?>/";
     	const websocket = "<?= WEBSOCKET ;?>";
     	const notify_status = <?= defined('notify') ? notify : 0 ;?>;
+
+    	//create firebase reference
+		const dbRef = new Firebase("<?= FIREBASEIO ;?>");
     </script>
 
 </head>
